3e9cad88f517b2273d4abe12267affee919d1db6
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-13 22:17:52 +0300

    Refactor rendering logic and add reactive state management
    Refactored the rendering logic in app.js to use the Ludis.js framework and the new reactive state management feature. Instead of directly setting the message variable, the state object is now used to store the message value. The renderApp function is introduced to render the updated message in the container element. Additionally, a setTimeout is added to demonstrate the reactivity by updating the message after 1 second.
    
    Closes #123

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-let message = "Ludis";
+let state = reactive({ message: "Hellow Ludis" });
 
-render("#container", `<h1>${message}</h1>`);
+function renderApp() {
+  render("#container", `<h1>${state.message}</h1>`);
+}
+
+renderApp();
+
+setTimeout(() => {
+  state.message = "Hello";
+}, 1000);
--- ludis.js
+++ ludis.js
@@ -4,3 +4,25 @@ const render = (element, content) => {
     app.innerHTML = content;
   }
 };
+
+const reactive = (obj) => {
+  const reactiveObj = {};
+
+  Object.keys(obj).forEach((key) => {
+    let value = obj[key];
+
+    Object.defineProperty(reactiveObj, key, {
+      get() {
+        console.log("Getting value of", key);
+        return value;
+      },
+      set(newValue) {
+        console.log("Setting value of", key, "to", newValue);
+        value = newValue;
+        renderApp();
+      },
+    });
+  });
+
+  return reactiveObj;
+};
