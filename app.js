c571c26716989976ca7c2f8f961876e9656f9be7
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-13 21:07:16 +0300

    Add Ludis.js framework and render function

---
+++ app.js
@@ -0,0 +1,3 @@
+let message = "Ludis";
+
+render("#container", `<h1>${message}</h1>`);
---
+++ ludis.js
@@ -0,0 +1,6 @@
+const render = (element, content) => {
+  const app = document.querySelector(element);
+  if (app !== null) {
+    app.innerHTML = content;
+  }
+};
